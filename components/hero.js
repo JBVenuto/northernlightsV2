236e6eadd71b2db9ceb6af62ad1d425dcc8cce33
Joseph Venuto <jbvenuto@gmail.com>
2020-07-29 16:07:41 -0400

    hero styling and imported images

--- components/hero.js
+++ components/hero.js
@@ -3,7 +3,7 @@ import styles from '../styles/hero.module.scss'
 const Hero = props => {
     return (
         <section className='hero'>
-            <div className='hero-last'>The last recorded Kp was {props.kp} at {props.time}</div>
+            <div className={styles.recent_kp}>The last recorded Kp was {props.kp} at {props.time}</div>
             <div className={styles.jumbotron}>
                 <p>find</p><p>your</p><p>lights</p>
             </div>
