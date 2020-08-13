c24b5ea653675d064efe58179be968ff237ff6c4
Joseph Venuto <jbvenuto@gmail.com>
2020-08-13 10:03:47 -0400

    added bar graph example, and fixed top bar

--- components/kp-bar.js
+++ components/kp-bar.js
@@ -12,14 +12,54 @@ class KpBar extends Component {
                                 kp: parseInt(kp[1])
                             }
                     }),
-            lastKp: 10
+            lastKp: 15,
+            x: null,
+            y: null
         }
+    }
+
+    componentDidMount() {
+        console.log('state:', this.state);
+        // const lastKp = this.state.kpData.length - 1;
+
+        this.setState({
+            // lastKp,
+            x: this.scaleX(),
+            y: this.scaleY()
+        })
+        console.log('state', this.state)
+    }
+
+    scaleX() {
+        return d3
+            .scaleLinear()
+            .domain([0, 9]) //max value
+            .range([0, this.state.width]);
+    }
+
+    scaleY() {
+        return d3
+            .scaleTime()
+            .domain([this.state.kpData[0][0], this.state.kpData[this.state.lastKp][0]])
+            .range([0, this.state.height]);
+    }
+
+    initGraph() {
+        const { x, y, kpData } = this.state;
+
+        return kpData.map((d, i) => (
+            <div key={i} className="bar-chart__div" style={{ width: x(d.kp), height: y.bandwidth() }}>
+                {d.kp}
+            </div>
+        ))
     }
 
     render() {
         return(
-            // <h3>Hello World!</h3>
-            <svg ref={r => (this.chartRef = r)} />
+            <h3>Hello World!</h3>
+            // <div>
+            //     {this.initGraph()}
+            // </div>
         )
     }
 }
--- pages/index.js
+++ pages/index.js
@@ -5,26 +5,25 @@ import KpGraph from '../components/kp-graph';
 import Line from '../components/line-graph';
 import KpBar from '../components/kp-bar';
 
-import { forcast } from '../lib/forcast';
 import { lastKp } from '../lib/previous';
+import { forcast } from '../lib/forcast';
 
 export async function getStaticProps() {
   const res = await fetch('https://services.swpc.noaa.gov/products/noaa-planetary-k-index-forecast.json')
   const data = await res.json()
   const upcoming = await forcast(data)
-  const lastRecorded = await lastKp(data)
-  console.log(lastRecorded)
+  const last = await lastKp(data)
 
   return {
     props: {
-      lastRecorded,
+      last,
       upcoming
     }
   }
 }
 
 
-export default function Home({ upcoming }) {
+export default function Home({ last, upcoming }) {
   return (
     <div>
       <Head>
@@ -33,7 +32,7 @@ export default function Home({ upcoming }) {
         <link href="https://fonts.googleapis.com/css?family=Germania+One|Roboto" rel="stylesheet"></link>
       </Head>
 
-      {/* <Hero time={lastRecorded[0]} kp={lastRecorded[1]} /> */}
+      <Hero time={last[0]} kp={last[1]} />
       <MapInfo />
       {/* <Line kp={upcoming} /> */}
       {/* <KpGraph kp={upcoming} /> */}
