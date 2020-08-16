8b2dd5fda78cd95885acfdbee6a31a93287a6e54
Joseph Venuto <jbvenuto@gmail.com>
2020-08-16 10:58:24 -0400

    Finished css classes for >4 kp

--- components/kp-bar.js
+++ components/kp-bar.js
@@ -5,10 +5,8 @@ const KpBar = props => {
         <section className={styles.kpBar}>
             {props.kp.map(kpTime => 
                 <div>
-                    {/* <div className={styles.kpBar_date}>{kpTime[0]}</div> */}
                     <p>{kpTime[0]}</p>
                     <span className={`kp_${kpTime[1]}`}>{kpTime[1]}</span>
-                    {/* <li>{kpTime[1]} KP at {kpTime[0]}</li> */}
                 </div>
             )} 
         </section>
